533fc3a002c5ac44d4e510ff92612ef1e6e51bfc
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2024-05-19 19:05:54 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Button variant='destructive'>Click me</Button>
+    </div>
+  );
+}
